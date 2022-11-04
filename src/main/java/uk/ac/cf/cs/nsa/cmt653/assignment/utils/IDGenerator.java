07d498dddbccac9a2aa4a24e66334b78c84af4fd
Group11 <ekongr@cardiff.ac.uk>
2022-11-04 13:45:46 +0100

    fix IDGenerator.java

--- src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/utils/IDGenerator.java
+++ src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/utils/IDGenerator.java
@@ -2,7 +2,7 @@ package uk.ac.cf.cs.nsa.cmt653.assignment.utils;
 
 public interface IDGenerator {
 
-    default <T extends Class<T>> int generate(T whichClass){
+    default <T extends Class<?>> int generate(T whichClass){
         return 0;
     }
 }
