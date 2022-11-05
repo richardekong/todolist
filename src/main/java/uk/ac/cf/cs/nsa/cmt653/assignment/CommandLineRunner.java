bbf938ab6d3609f02afcd8660a929b1f9080053c
Group11 <ekongr@cardiff.ac.uk>
2022-11-05 10:33:06 +0100

    modify this class

--- src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/CommandLineRunner.java
+++ src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/CommandLineRunner.java
@@ -1,7 +1,13 @@
 package uk.ac.cf.cs.nsa.cmt653.assignment;
 
+import uk.ac.cf.cs.nsa.cmt653.assignment.manager.TodoManager;
+import uk.ac.cf.cs.nsa.cmt653.assignment.repository.TodoRepository;
+import uk.ac.cf.cs.nsa.cmt653.assignment.util.Command;
+
 public class CommandLineRunner {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+        TodoRepository repo = new TodoManager();
+        Command.listAllTodoNames.setExecutionBlock(repo::listTodos);
     }
 }
