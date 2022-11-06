d3e83047b449fab6cdcaa615d1ab82526ed59393
Group11 <ekongr@cardiff.ac.uk>
2022-11-06 20:27:53 +0100

    fixed

--- src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/model/Task.java
+++ src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/model/Task.java
@@ -4,39 +4,26 @@ import java.time.Duration;
 import java.time.LocalTime;
 
 public class Task {
-    private int id;
     private String description;
     private final LocalTime startTime;
     private Duration deadlineInMinutes;
     private Status status;
     private static final Long defaultDeadLineInMinutes = 10L;
 
-    public Task(){
-        id = 0;
-        description = "Task-"+id;
+    public Task() {
+        description = "Anonymous Task";
         deadlineInMinutes = Duration.ofMinutes(defaultDeadLineInMinutes);
         startTime = LocalTime.now();
         status = Status.unDone;
     }
+
     public Task(String description, Long deadlineInMinutes) {
-        this.id = 0;
         this.description = description;
         this.deadlineInMinutes = (deadlineInMinutes > 0L)
                 ? Duration.ofMinutes(deadlineInMinutes)
                 : Duration.ofMinutes(defaultDeadLineInMinutes);
         this.startTime = LocalTime.now();
         this.status = Status.unDone;
-    }
-
-    public void setID(int id){
-        if (id <= 0 ){
-            System.err.println("Invalid ID. ID must be > 0!");
-            return;
-        }
-        this.id = id;
-    }
-    public int getId() {
-        return id;
     }
 
     public void setDescription(String description) {
