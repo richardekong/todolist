fe4e3859f3f59986ba3a39a4524b7618ab5679dd
Group11 <ekongr@cardiff.ac.uk>
2022-11-06 20:33:55 +0100

    fixed

--- src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/model/Todo.java
+++ src/main/java/uk/ac/cf/cs/nsa/cmt653/assignment/model/Todo.java
@@ -5,7 +5,6 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Todo {
-    private  int id;
     private String name;
     private LinkedList<Task> tasks;
     private final LocalTime createAt;
@@ -13,30 +12,17 @@ public class Todo {
     private static final LinkedList<Task> defaultTasks = new LinkedList<>(List.of(new Task(), new Task()));
 
     public Todo() {
-        id = 0;
-        name = this.getClass().getSimpleName() + "-" + id;
+        name = "New-" + this.getClass().getSimpleName();
         tasks = defaultTasks;
         createAt = LocalTime.now();
     }
 
     public Todo(String name, LinkedList<Task> tasks){
-        id = 0;
         this.name = name;
         this.tasks = tasks;
         createAt = LocalTime.now();
     }
 
-    public void setId(int id) {
-        if (id <= 0 ){
-            System.err.println("Invalid ID, ID must be > 0!");
-            return;
-        }
-        this.id = id;
-    }
-
-    public int getId(){
-        return id;
-    }
     public void setName(String name){
         this.name = name;
     }
